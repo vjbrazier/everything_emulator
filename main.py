61b480a731f2531a33ac80e8dc1a136e4a3a748d
IridescentBlaze <iridescentblazer@gmail.com>
2025-02-27 22:21:14 -0500

    Renamned variables for clarity Adjust styling to make it more readable The arrows for the console scroller work now Created a paths file that will store file locations

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # Import
-import eel, os, json
+import eel, os, json, hashlib
 
 # Initialize
 eel.init('web')
 
 # List of consoles
-consoles = ['Wii', 'NES', 'SNES', 'Switch', 'Gameboy', 'Console', 'Console2', 'Console3']
+consoles = ['XBOX', 'XBOX 360', 'Gamecube', 'Wii', 'Nintendo 64', 'Nintendo Switch', 'Super Nintendo', 'Wii U']
+
+# List of games
+games = ['Super Mario World', 'Mario Odyssey', 'Mario Kart', 'Mario Party', 'Mario 64', 'Mario Galaxy', "Luigi's Mansion"]
+
+
+
+# Dummy data
+for i in range(25):
+    games.append(f"Game {i}")
 
 # Passes consoles
 @eel.expose
 def get_consoles():
     return consoles
+
+@eel.expose
+def get_games():
+    return games
 
 # Run the program
 if __name__ == '__main__':
--- web/script.js
+++ web/script.js
@@ -1,30 +1,92 @@
+// Variables/Site Elements
+const consoles           = document.getElementById('consoles');
+let   consoles_margin    = parseInt(window.getComputedStyle(consoles).marginLeft);
+const games              = document.getElementById('games');
+
+// Creates all of the consoles
 async function loadConsoles() {
-    let consoles = await eel.get_consoles()();
-    let console_list = document.getElementById('consoles');
 
-    for (let i = 0; i < consoles.length; i++) {
-        let button = document.createElement('button');
-        button.classList.add('console');
+    let console_list = await eel.get_consoles()();
 
-        if (i == 0) {
-            button.classList.add('carosel-0');
-        } else if (i == 1) {
-            button.classList.add('carosel-1');
-        } else if (i == 2) {
-            button.classList.add('carosel-2');
-        } else if (i == 3) {
-            button.classList.add('carosel-3');
-        } else if (i == 4) {
-            button.classList.add('carosel-4');
-        }
+    for (let i = 0; i < console_list.length; i++) {
+        console_id = console_list[i].replaceAll(' ', '-').toLowerCase();
 
-        button.id = consoles[i].toLowerCase();
-        button.innerText = consoles[i];
-        console_list.appendChild(button);
+        var console_div    = document.createElement('div');
+        var console_button = document.createElement('button');
+        var console_text   = document.createElement('h3');
+
+        console_div.classList.add('console-div');
+        console_button.classList.add('console');
+        console_text.classList.add('console-text');
+
+        console_div.id    = console_id + '-div';
+        console_button.id = console_id;
+        console_text.id   = console_id + '-text';
+
+        console_button.innerText = console_list[i];
+        console_text.innerText   = console_list[i];
+
+        console_div.appendChild(console_button);
+        console_div.appendChild(console_text);
+        consoles.appendChild(console_div);
     }
 
 }
 
+//Cycles through the consoles
+function visbility_check(id) {
+    var element = document.getElementById(id);
 
+    if ((parseInt(window.getComputedStyle(element).marginLeft) < 0)) {
+        element.classList.remove('visible');
+    }
+}
 
-window.onload = loadConsoles;
+document.getElementById('console-left').addEventListener('click', () => {
+    consoles_margin -= 250;
+    consoles.style.marginLeft = consoles_margin + 'px';
+})
+
+document.getElementById('console-right').addEventListener('click', () => {
+    consoles_margin += 250;
+    consoles.style.marginLeft = consoles_margin + 'px';
+})
+
+// Loads up all the games
+async function loadGames() {
+    let game_list = await eel.get_games()();
+
+    for (let i = 0; i < game_list.length; i++) {
+        game_id = game_list[i].replaceAll(' ', '-').toLowerCase();
+
+        var game_div     = document.createElement('div');
+        var game_button  = document.createElement('button');
+        var game_text    = document.createElement('h3');
+        var game_console = document.createElement('h3');
+
+        game_div.id     = game_id + '-div';
+        game_button.id  = game_id;
+        game_text.id    = game_id + '-text';
+        game_console.id = game_id + '-div';
+
+        game_div.classList.add('game-div');
+        game_button.classList.add('game');
+        game_text.classList.add('game-text');
+        game_console.classList.add('game-console');
+
+        game_button.innerText  = game_list[i];
+        game_text.innerText    = game_list[i];
+        game_console.innerText = 'Console';
+
+        game_div.appendChild(game_button);
+        game_div.appendChild(game_console);
+        game_div.appendChild(game_text);
+        games.appendChild(game_div);
+    }
+}
+
+// Calls necessary functions once the page has loaded
+document.addEventListener('DOMContentLoaded', () => {
+    loadConsoles();
+    loadGames();
+})
